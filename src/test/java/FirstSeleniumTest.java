69feb18f25ce3e8dcc72b0f09bc147e929e58cbd
Andrejs Ap <dogma21@inbox.lv>
2022-10-26 21:01:54 +0300

    Starting browser, opening page and push accept btn(adding web driver/waiting with locator, finding element on page)

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,12 +1,28 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class FirstSeleniumTest {
+    private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
+
     @Test
     public void openHomePage() {
-        System.setProperty("webdriver.chrome.driver","C://chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         WebDriver browser = new ChromeDriver();
+        browser.manage().window().maximize();
         browser.get("http://discovercars.com/");
+
+        WebDriverWait wain = new WebDriverWait(browser, Duration.ofSeconds(10));
+
+        wain.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
+
+
+        browser.findElement(ACCEPT_COOKIES_BTN).click();
+
     }
 }
